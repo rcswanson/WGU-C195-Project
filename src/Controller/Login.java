296d69f9e061b0056741f59ed5c50dff136d4485
Rick Swanson <98424126+rcswanson@users.noreply.github.com>
2022-08-09 10:49:16 -0500

    Program successfully runs login window

--- src/Controller/Login.java
+++ src/Controller/Login.java
@@ -1,4 +1,27 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Login {
+
+    static Stage stage;
+    static Parent scene;
+
+    public TextField loginID;
+    public TextField loginPass;
+    public Button loginButton;
+    public Text loginTimeZone;
+
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,13 +1,27 @@
 package Main;
 
 import Helper.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage login) throws Exception {
+        Parent root = FXMLLoader.load((getClass().getResource("/View/Login.fxml")));
+        login.setTitle("Client Schedule");
+        login.setScene(new Scene(root, 500, 300));
+        login.show();
+    }
 
     public static void main(String[] args) {
-	// write your code here
-
+        // write your code here
         JDBC.openConnection();
+
+        launch(args);
 
         JDBC.closeConnection();
 
