92635072a45109d545fb28399cbd8f1b37f9ab06
Rick Swanson <98424126+rcswanson@users.noreply.github.com>
2022-11-08 16:15:27 -0600

    Reports Tab Controller added

---
+++ src/Utilities/DivisionSql.java
@@ -0,0 +1,88 @@
+package Utilities;
+
+import Main.JDBC;
+import Model.Country;
+import Model.Division;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static Main.JDBC.connection;
+
+
+public class DivisionSql {
+
+    // OBSERVABLE LIST OF DATA ENTRIES IN CUSTOMERS SCHEMA
+    public static ObservableList<Division> divisions = FXCollections.observableArrayList();
+
+    // EXECUTES SQL STATEMENT TO RETRIEVE ALL DATA FROM SCHEMA
+    public static ObservableList<Division> getDivisions() throws SQLException {
+        try {
+            PreparedStatement pStmt = connection.prepareStatement("SELECT * FROM first_level_divisions");
+            ResultSet rs = pStmt.executeQuery();
+            while (rs.next()) {
+                Division newDivision = new Division(
+                        rs.getInt("Division_ID"),
+                        rs.getString("Division"),
+                        rs.getInt("Country_ID"));
+                divisions.add(newDivision);
+            }
+            return divisions;
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        return null;
+    }
+
+    // EXECUTES SQL STATEMENT TO RETRIEVE DATA FROM SCHEMA TO MATCH ID
+    public static Division getDivisionId(String division) throws SQLException {
+        PreparedStatement pStmt = connection.prepareStatement("SELECT * FROM first_level_divisions WHERE Division=?");
+
+        pStmt.setString(1, division);
+
+        try {
+            pStmt.execute();
+            ResultSet resultSet = pStmt.getResultSet();
+
+            while (resultSet.next()) {
+                Division newDivision = new Division(
+                        resultSet.getInt("Division_ID"),
+                        resultSet.getString("Division"),
+                        resultSet.getInt("COUNTRY_ID")
+                );
+                return newDivision;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return null;
+    }
+
+
+    // EXECUTES SQL STATEMENT RETRIEVING DIVISION THAT MATCHES SELECTED COUNTRY
+    public static ObservableList<Division> getDivisionsByCountry(String country) throws SQLException {
+        Country newCountry = CountrySql.getCountryId(country);
+        PreparedStatement pStmt = connection.prepareStatement("SELECT * FROM first_level_divisions WHERE Country_ID=?");
+        pStmt.setInt(1, newCountry.getCountryId());
+        try {
+            pStmt.execute();
+            ResultSet rs = pStmt.getResultSet();
+            while (rs.next()) {
+                Division newDivision = new Division(
+                        rs.getInt("Division_ID"),
+                        rs.getString("Division"),
+                        rs.getInt("Country_ID"));
+                divisions.add(newDivision);
+            }
+            return divisions;
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        return null;
+    }
+}
+
